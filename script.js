82343dd4ea193504233061a0d33ffd4a7f5022a7
Travis Anderson <travis.anderson30@gmail.com>
2024-02-27 07:43:17 -0700

    Create operate function to call the appropriate operator function

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+let num1;
+let num2;
+let op;
+
+function operate(operator, number1, number2) {
+    switch (operator) {
+        case "+":
+            return add(number1, number2)
+        case "-":
+            return subtract(number1, number2);
+        case "*":
+            return multiply(number1, number2);
+        case "/":
+            return divide(number1, number2);
+        default:
+            break;
+    }
+}
+
 function add(a, b) {
     return a + b;
 }
